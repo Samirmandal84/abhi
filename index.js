ddebf8e756d9b5325474c8cde9128aab72ad749f
Samir Mandal <113394127+Samirmandal84@users.noreply.github.com>
2023-01-23 22:03:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 alert('Hi CUTIE❤️🥰');
 alert('❤️ Happy Birthday My❤️');
 alert('The more you hurt me, the more I will love you');
-alert('🫶Love You Always🫶');
+alert('😊Love You Always❤️');
 alert('I will be there for You Always');
-alert('🫶 Never ever Leave You🥺');
+alert('❤️Never ever Leave You🥺');
