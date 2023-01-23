404e9084856ae0547f58c0d9c07cfa08db68cc03
Samir Mandal <113394127+Samirmandal84@users.noreply.github.com>
2023-01-23 21:59:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+alert('Hi CUTIE❤️🥰');
+alert('❤️ Happy Birthday My❤️');
+alert('The more you hurt me, the more I will love you');
+alert('🫶Love You Always🫶');
+alert('I will be there for You Always');
+alert('🫶 Never ever Leave You🥺');
